8a2eb83321c396ff31fe9139f1494233b14b05a0
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-13 19:37:32 +0500

    code review

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const app = require('express')();
-const multer = require('multer');
+const express = require('express');
 
-// const upload = multer({dest: 'uploads/'});
+const { upload } = require('./services/upload.service');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        return cb(null, './uploads');
-    },
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}-${file.originalname}`)
-    }
+const app = express()
+
+app.post('/upload', upload.single('gingka'), (req, res) => {
+    res.status(201).json({ "bingo": "file uploaded successfullyyy...." });
 });
 
-const upload = multer({storage});
-app.post('/upload', upload.single('gingka'), (req, res)=>{
-    res.status(201).json({"bingo":"file uploaded successfullyyy...."});
-});
-
-app.listen(4000, ()=>{ console.log("server up and running on PORT:4000")});
+app.listen(4000, () => { console.log("server up and running on PORT:4000") });
